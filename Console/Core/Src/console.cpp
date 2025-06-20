ad48dcbd475448d686bae5243ab7fa4511bb0940
KnapMartin <martin_knap@yahoo.com>
2025-06-20 15:41:40 +0200

    update console

--- Console/Core/Src/console.cpp
+++ Console/Core/Src/console.cpp
@@ -77,6 +77,7 @@ void Console::processLine(const char *line)
     if (line == nullptr || line[0] == '\0')
     {
         prompt();
+        print("\r\n");
         return;
     }
 
@@ -91,6 +92,7 @@ void Console::processLine(const char *line)
         {
             handler(line); // Call the command handler
             prompt();      // Prompt for the next command
+            print("\r\n"); // Print a new line after command execution
             return;
         }
     }
